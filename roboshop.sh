40f95b8fe39bfd2741e026e3b772919a1d9a9a21
Nandu-Addanki1999 <nandua1999@gmail.com>
2025-05-23 00:53:08 +0530

    colors

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-087a68b55bf83fdd3" #replace with your SG ID
+INSTANCES=("mongodb" "mysql "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "diapatch" "frontend")
+ZONE_ID="Z09414452UFBO39K2SLBL"
+DOMAIN_NAME="nandu.sbs" 
+
+for instance in ${INSTANCES[@]}
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-087a68b55bf83fdd3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP address: $IP"
+done
